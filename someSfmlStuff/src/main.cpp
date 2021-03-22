e2197a45fe60f0aef4163f0d9686e345c331b272
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-03-22 20:51:50 +0000

    setup sfml2.5 project example linux

---
+++ someSfmlStuff/src/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
