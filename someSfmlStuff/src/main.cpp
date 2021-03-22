472ecdb08fe33cf33bdca14b54e18fa0ace44a3b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-03-22 21:03:38 +0000

    drawing circles on screen

--- someSfmlStuff/src/main.cpp
+++ someSfmlStuff/src/main.cpp
@@ -1,9 +1,23 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+
+using Shapes = std::vector<sf::Shape *>;
+
+void addCircle(Shapes &shapes, sf::Vector2i clickPosition)
+{
+    auto circle = new sf::CircleShape(10);
+    circle->setOrigin(10, 10);
+    circle->setFillColor(sf::Color::White);
+    circle->setPosition(clickPosition.x, clickPosition.y);
+
+    shapes.push_back(circle);
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
+    Shapes shapes;
 
     while (window.isOpen())
     {
@@ -14,7 +28,17 @@ int main()
                 window.close();
         }
 
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            auto pos = sf::Mouse::getPosition(window);
+            addCircle(shapes, pos);
+        }
+
         window.clear(sf::Color::Black);
+        for (auto &s : shapes)
+        {
+            window.draw(*s);
+        }
         window.display();
     }
 
