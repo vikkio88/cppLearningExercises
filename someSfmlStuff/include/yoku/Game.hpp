517cde48e4a3f33cd32978815cdcb5668c13a71c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-04-05 13:12:58 +0100

    moved window size to Window wrapper

--- someSfmlStuff/include/yoku/Game.hpp
+++ someSfmlStuff/include/yoku/Game.hpp
@@ -15,8 +15,6 @@ namespace yoku
         Window m_Window;
         sf::Clock m_Clock;
         SceneManager m_SceneManager;
-        int m_Width;
-        int m_Height;
 
     public:
         Game(
@@ -24,7 +22,7 @@ namespace yoku
             int width = 800,
             int height = 600,
             sf::Uint32 style = sf::Style::Titlebar)
-            : m_Window(name, width, height, style), m_Name(name), m_Width(width), m_Height(height)
+            : m_Window(name, width, height, style), m_Name(name)
         {
         }
 
--- someSfmlStuff/include/yoku/Window.hpp
+++ someSfmlStuff/include/yoku/Window.hpp
@@ -8,6 +8,8 @@ namespace yoku
     {
     private:
         sf::RenderWindow m_Renderer;
+        int m_Width;
+        int m_Height;
 
     public:
         Window(const std::string &title, int width, int height, sf::Uint32 style);
@@ -16,6 +18,8 @@ namespace yoku
 
         sf::Event getEvent();
         sf::Vector2i getMousePosition();
+        int getWidth() { return m_Width; }
+        int getHeight() { return m_Height; }
 
         void startDraw();
         void draw(sf::Drawable &drawable);
