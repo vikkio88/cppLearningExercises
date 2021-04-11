a7e08433ec6ffb45e23abe67f3c6fe6d532abefc
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-04-11 21:41:11 +0100

    removed GLEW as it was pointless

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -1,56 +1,53 @@
-#include <iostream>
-#define GLEW_STATIC
-#include <GL/glew.h>
-
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 
-const GLint WIDTH = 800, HEIGHT = 600;
-
 int main()
 {
-    sf::ContextSettings settings;
-    settings.depthBits = 24;
-    settings.stencilBits = 8;
-    // OpenGl Version => glxinfo | grep "OpenGL version"
-    // 4.6 on my laptop
-    settings.majorVersion = 4;
-    settings.minorVersion = 6;
-    settings.attributeFlags = sf::ContextSettings::Core;
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "GlewTest", sf::Style::Titlebar, settings);
-    glewExperimental = GL_TRUE;
-
+    sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Titlebar, sf::ContextSettings(32));
+    window.setVerticalSyncEnabled(true);
     window.setActive(true);
 
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "Problems initialising GLEW/OPENGL\n";
-        return EXIT_FAILURE;
-    }
-
-    glViewport(0, 0, WIDTH, HEIGHT);
-    glMatrixMode(GL_PROJECTION);
+    sf::Event event;
 
     while (window.isOpen())
     {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         {
             window.close();
         }
-
-        glClearColor(.3f, .2f, .5f, .9f);
+        // inside the main loop
+        glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glColor3f(0.0f, 0.0f, 1.0f);
-
-        glBegin(GL_LINE_LOOP);
-        glVertex3f(-1.0f, 0.0f, 0.0f);
-        glVertex3f(0.0f, -1.0f, 0.0f);
-        glVertex3f(1.0f, 0.0f, 0.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 1.f, 1.f);
+        glVertex3f(-.9f, -.5f, 10.f);
+        glVertex3f(0.0f, 0.5f, 0.5f);
+        glVertex3f(1.f, -0.5f, 0.f);
         glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(0.f, 0.f, 0.f);
+        glVertex3f(0.f, 0.f, 20.f);
+
+        glVertex3f(0.f, 0.f, 0.f);
+        glVertex3f(0.f, 20.f, 0.f);
+
+        glVertex3f(0.f, 0.f, 0.f);
+        glVertex3f(20.f, 0.f, 0.f);
+        glEnd();
+
+        // end the current frame (internally swaps the front and back buffers)
         window.display();
     }
 
-    std::cout << "\n\nBye, have a good time\n";
     return EXIT_SUCCESS;
 }
